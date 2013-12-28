f3ff7c8bcfa8c3d136a910ae27241a0779f1ac51
Nicole Gaskari <nicole_gaskari@mba.berkeley.edu>
2013-12-28 18:25:04 +0000

    Changed home page path

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,13 +4,13 @@ describe "Static Pages" do
   describe "Home page" do
     it "should have the content 'Demo App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
+      visit root_path
       expect(page). to have _content ('Demo App')
     end
   end
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
   end
