e7bf56d0709fcc127ec2c9175221fbbf39550266
Nicole Gaskari <nicole_gaskari@mba.berkeley.edu>
2013-12-28 19:59:49 +0000

    crushing it

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
-
-describe "Static Pages" do
-  describe "Home page" do
+ 
+describe "Static pages" do
+ describe "Home page" do
+ 
     it "should have the content 'Demo App'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit root_path
-      expect(page). to have _content ('Demo App')
+      expect(page).to have_content('Demo App')
     end
   end
   describe "Help page" do
